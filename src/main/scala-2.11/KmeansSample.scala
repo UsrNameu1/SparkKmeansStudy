2074a87f43378c9fa6e4c1036f8f75a7207bf4ec
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2015-07-12 20:54:21 +0900

    add sample for eucledian measure

--- src/main/scala-2.11/KmeansSample.scala
+++ src/main/scala-2.11/KmeansSample.scala
@@ -6,7 +6,6 @@ import org.apache.spark.SparkContext
 import org.apache.spark.mllib.clustering.KMeans
 import org.apache.spark.mllib.feature.HashingTF
 import org.apache.spark.mllib.feature.IDF
-import org.slf4j.Logger
 
 object KmeansSample extends App {
   val context = new SparkContext("local", "reuter-demo")
@@ -14,27 +13,46 @@ object KmeansSample extends App {
   val documents = context
     .wholeTextFiles("src/main/resources/reuter/extracted/")
     .map{ _._2 }
-//    .map{ text => text.lines }
-//    .map{ lines => lines.drop(lines.size - 2).next() }
-    .map{ _.split(" ").toSeq }
+    .map{ text => text.split("\n\n").last }
+    .map{ _.split("\\s+").toSeq }
 
   val hashingTF = new HashingTF()
   val tf = hashingTF.transform(documents)
 
-  tf.cache()
   val idf = new IDF().fit(tf)
   val tfidf = idf.transform(tf)
 
   val k = 20
   val maxItreations = 50
-  val clusters = KMeans.train(tfidf, k, maxItreations)
+  val kmeansModel = KMeans.train(tfidf, k, maxItreations)
 
-//  clusters.clusterCenters.foreach {
-//    center => println(f"${center.toArray.mkString("[", ", ", "]")}%s")
-//  }
-//
-//  tfidf.foreach { tuple =>
-//    println(f"${tuple._2.toArray.mkString("[", ", ", "]")}%s " +
-//      f"(${tuple._1}%s) : cluster = ${clusters.predict(tuple._2)}%d")
-//  }
+  (kmeansModel.predict(tfidf) zip documents).groupBy {
+    case (clusterId: Int, _) => clusterId
+  }.sortBy {
+    case (clusterId: Int, pairs: Iterable[(Int, Seq[String])]) => clusterId
+  }.map {
+    case (clusterId: Int, pairs: Iterable[(Int, Seq[String])]) => (clusterId, pairs.map{ _._2 } )
+  }.map {
+    case (clusterId: Int, docs: Iterable[Seq[String]]) => (clusterId, docs.flatten)
+  }.map {
+    case (clusterId: Int, words: Iterable[String]) => {
+      var wordsMap = Map[String, Int]()
+      words.foreach { word =>
+        val oldCount = wordsMap.get(word) getOrElse 0
+        wordsMap += (word -> (oldCount + 1))
+      }
+      (clusterId, wordsMap.toSeq.sortBy{ _._2 }(Ordering[Int].reverse))
+    }
+  }.map {
+    case (clusterId: Int, wordsTable: Seq[(String, Int)]) => (clusterId, wordsTable.take(10))
+  }.foreach {
+    case (clusterId: Int, wordsTable: Seq[(String, Int)]) => {
+      val wordCountString = wordsTable.map {
+        case (word, count) => s"${word}(${count})"
+      }.reduceLeft {
+        (acc: String, elem: String) => (acc + ", " + elem)
+      }
+      println(s"ClusterId : ${clusterId}, Top words : ${wordCountString}")
+    }
+  }
 }
